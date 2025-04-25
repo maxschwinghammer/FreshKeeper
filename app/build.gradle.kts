e1411f4c600775b472d721b55a0fdb9356d9bc4c
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2025-04-25 02:02:13 +0200

    Updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
