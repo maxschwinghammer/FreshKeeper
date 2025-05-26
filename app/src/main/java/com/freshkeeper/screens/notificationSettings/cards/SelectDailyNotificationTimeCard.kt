d78798df21ed65394d75e50a14ecfe3c867b3825
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2025-05-26 08:47:42 +0200

    Style adaption

--- app/src/main/java/com/freshkeeper/screens/notificationSettings/cards/SelectDailyNotificationTimeCard.kt
+++ app/src/main/java/com/freshkeeper/screens/notificationSettings/cards/SelectDailyNotificationTimeCard.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.freshkeeper.R
 import com.freshkeeper.screens.notificationSettings.TimePickerDialog
@@ -65,8 +68,13 @@ fun SelectDailyNotificationTimeCard(
                     .padding(16.dp),
         ) {
             Column(modifier = Modifier.weight(1f)) {
-                Text(text = stringResource(R.string.select_daily_notification_time))
+                Text(
+                    text = stringResource(R.string.select_daily_notification_time),
+                    maxLines = 2,
+                    overflow = TextOverflow.Clip,
+                )
             }
+            Spacer(modifier = Modifier.width(20.dp))
             Image(
                 painter = painterResource(R.drawable.time),
                 contentDescription = "Time Icon",
